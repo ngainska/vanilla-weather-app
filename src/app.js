2868891b4989da1d7c97e2e9a50916702fefc354
ngainska <87204580+ngainska@users.noreply.github.com>
2021-08-16 18:27:51 +0100

    Show current temp

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 function displayTemp(response){
     console.log(response.data)
-
+let mainTemp = document.querySelector("#main-temp");
+mainTemp.innerHTML=Math.round(response.data.main.temp);
+let mainCity = document.querySelector("#city");
+mainCity.innerHTML= response.data.name;
+let mainDescription = document.querySelector("#main-description");
+mainDescription.innerHTML = response.data.weather[0].description;
+let precipitation = document.querySelector("#precipitation");
+precipitation.innerHTML=response.data.main.precipitation;
+let humidity = document.querySelector("#humidity");
+humidity.innerHTML=response.data.main.humidity;
+let wind = document.querySelector("#wind");
+wind.innerHTML=Math.round(response.data.wind.speed);
 }
 
 

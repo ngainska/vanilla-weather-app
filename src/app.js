85bf5f97736a0030737b462adc71204e2e50f3e5
ngainska <87204580+ngainska@users.noreply.github.com>
2021-08-16 18:45:41 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+ "Sunday",
+ "Monday",
+  "Tuesday",
+ "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}:${minutes} `;
+}
+
 function displayTemp(response){
     console.log(response.data)
 let mainTemp = document.querySelector("#main-temp");
@@ -12,6 +30,8 @@ let humidity = document.querySelector("#humidity");
 humidity.innerHTML=response.data.main.humidity;
 let wind = document.querySelector("#wind");
 wind.innerHTML=Math.round(response.data.wind.speed);
+let date = document.querySelector("#date");
+date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 

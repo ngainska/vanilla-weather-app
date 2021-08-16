91569d0d17d6ecaee13fc8ca99acd85cdc5e68e3
ngainska <87204580+ngainska@users.noreply.github.com>
2021-08-16 20:11:34 +0100

    Get main icon

--- src/app.js
+++ src/app.js
@@ -32,10 +32,15 @@ let wind = document.querySelector("#wind");
 wind.innerHTML=Math.round(response.data.wind.speed);
 let date = document.querySelector("#date");
 date.innerHTML = formatDate(response.data.dt * 1000);
+let mainIcon = document.querySelector("#main-icon");
+let getIcon = response.data.weather[0].icon;
+mainIcon.setAttribute(`src`,`http://openweathermap.org/img/wn/${getIcon}@2x.png` );
+mainIcon.setAttribute(`alt`,response.data.weather[0].description);
 }
 
 
+let city = "Glasgow";
 let apiKey = "b6a67f67579bcd300971f2f49b91d214";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Glasgow&appid=${apiKey}&units=metric`;
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiURL).then(displayTemp);
 
